a882efde4412938adb12c65bee0409d7f54594f6
Samuel Birky <sbirky21@gmail.com>
2019-09-19 14:50:06 -0500

    details works for books

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -1,4 +1,5 @@
 from .books.list import book_list
+from .books.details import book_details
 from .librarians.list import list_librarians
 from .libraries.list import list_libraries
 from .books.form import book_form
