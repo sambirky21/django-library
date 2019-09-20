a77bab87393d5df6136f3a3f4eef855c8223452d
Samuel Birky <sbirky21@gmail.com>
2019-09-20 13:47:56 -0500

    able to edit books

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -1,6 +1,7 @@
 from .books.list import book_list
 from .books.details import book_details
 from .books.form import book_form
+from .books.form import book_form, book_edit_form
 from .libraries.list import list_libraries
 from .libraries.details import library_details
 from .libraries.form import library_form
